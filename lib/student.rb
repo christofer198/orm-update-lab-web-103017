70e04c8542ec833ce506079372699c895a00550c
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 19:41:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def self.create_table
-    DB[:conn].execute("create table students()")
+    DB[:conn].execute("create table students(id integer primary key, name text, grade text)")
   end
 
 end
